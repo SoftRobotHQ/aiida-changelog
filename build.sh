6810510fe1ddeafc1edb485d0f741734f0589a05
DrewPerkinsSoftRobot <drew.perkins@softrobot.io>
2022-02-04 16:19:27 +0100

    add to readme

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-## Initial build
 rsync --update -raz --progress ../site ./
 
-## Make new site docs
 echo 'Making new site docs...'
 mkdocs build
 rm -R ../site
 cp -R ./site ../site
 echo 'DONE'
 
-## Remove old site docs
 echo 'Removing old site docs...'
 rm -rfv ./assets/*
 rm -rfv ./css/*
@@ -19,7 +15,6 @@ rm 404.html
 rm index.html
 echo 'DONE'
 
-## Move new site docs
 echo 'Moving new site docs...'
 mv ../site/assets/ ./assets
 mv ../site/css/ ./css
