1deb6446b08774336e6b8a5b4287adce88282219
DrewPerkinsSoftRobot <drew.perkins@softrobot.io>
2022-02-04 17:48:46 +0100

    update build script

--- build.sh
+++ build.sh
@@ -16,9 +16,9 @@ rm index.html
 echo 'DONE'
 
 echo 'Moving new site docs...'
-mv ../site/assets/ ./assets
-mv ../site/css/ ./css
-mv ../site/search/ ./search
+mv ../site/assets/* ./assets
+mv ../site/css/* ./css
+mv ../site/search/* ./search
 mv ../site/404.html .
 mv ../site/index.html .
 echo 'DONE'
