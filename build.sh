5cd5eeab28f79ab7d3a136ea9c87668fc76dd04c
DrewPerkinsSoftRobot <drew.perkins@softrobot.io>
2022-02-04 16:01:00 +0100

    automate the changelog

--- build.sh
+++ build.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+## Initial build
 rsync --update -raz --progress ../site ./
+
+## Make new site docs
+echo 'Making new site docs...'
+mkdocs build
+rm -R ../site
+cp -R ./site ../site
+echo 'DONE'
+
+## Remove old site docs
+echo 'Removing old site docs...'
+rm -rfv ./assets/*
+rm -rfv ./css/*
+rm -rfv ./search/*
+rm 404.html
+rm index.html
+echo 'DONE'
+
+## Move new site docs
+echo 'Moving new site docs...'
+mv ../site/assets/* ./assets
+mv ../site/css/* ./css
+mv ../site/search/* ./search
+mv ../site/404.html .
+mv ../site/index.html .
+
+echo 'DONE'
